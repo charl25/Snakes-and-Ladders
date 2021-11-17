75ee4a4be890e2e44eab8b146420f746e10c8eda
charl25 <sirayisinekhaya@gmail.com>
2021-11-17 10:23:32 +0200

    multiplayer mode

--- src/main/java/SnakesAndLadders.java
+++ src/main/java/SnakesAndLadders.java
@@ -2,85 +2,102 @@ import java.util.HashMap;
 
 public class SnakesAndLadders {
 
-    HashMap<String, Integer> player = new HashMap<>();
-    int play = 0;
+    HashMap<String, Integer> player = new HashMap<>(); // list of players and they positions on the board
+    HashMap<String, Integer> playerStatus = new HashMap<>(); // multiplayer states
 
-    public SnakesAndLadders(){
-        this.player.put("Player1",0);
-    }
+    public SnakesAndLadders(int mode){
 
-    public void throwDie(){
-        int dieTose = (int)(Math.random()*6)+1;
-        System.out.println("die :"+dieTose);
-        switch (this.play){
-            case 1:
-                play(dieTose);
-                break;
-            case 0:
-                if(dieTose == 6){
-                    this.play = 1;
-                }
-                break;
+        for(int i = 1; i<= mode;i++){
+            String player = "player" +i;
+            this.player.put(player,0);
+            this.playerStatus.put(player,0);
         }
     }
 
-    public void play(int moves){
-        int position = this.player.get("Player1")+moves;
+    public void throwDie(int playing){
+            String player = "player" +playing;
+            int dieTose = (int)(Math.random()*6)+1;
+            System.out.println("die :"+dieTose);
+            switch (this.playerStatus.get(player)){
+                case 1:
+                    play(player,dieTose);
+                    break;
+                case 0:
+                    if(dieTose == 6){
+                        this.playerStatus.replace(player,1);
+                    }
+                    break;
+            }
+            position(player);
+    }
+
+    public void play(String player, int moves){
+        int position = this.player.get(player)+moves;
 
         switch (position){
             case 1:
-                this.player.replace("Player1",38);
+                this.player.replace(player,38);
                 break;
             case 4:
-                this.player.replace("Player1",14);
+                this.player.replace(player,14);
                 break;
             case 8:
-                this.player.replace("Player1",30);
+                this.player.replace(player,30);
                 break;
             case 21:
-                this.player.replace("Player1",42);
+                this.player.replace(player,42);
                 break;
             case 28:
-                this.player.replace("Player1",76);
+                this.player.replace(player,76);
                 break;
             case 32:
-                this.player.replace("Player1",10);
+                this.player.replace(player,10);
                 break;
             case 36:
-                this.player.replace("Player1",6);
+                this.player.replace(player,6);
                 break;
             case 48:
-                this.player.replace("Player1",26);
+                this.player.replace(player,26);
                 break;
             case 50:
-                this.player.replace("Player1",67);
+                this.player.replace(player,67);
                 break;
             case 62:
-                this.player.replace("Player1",18);
+                this.player.replace(player,18);
                 break;
             case 71:
-                this.player.replace("Player1",92);
+                this.player.replace(player,92);
                 break;
             case 80:
-                this.player.replace("Player1",99);
+                this.player.replace(player,99);
                 break;
             case 88:
-                this.player.replace("Player1",24);
+                this.player.replace(player,24);
                 break;
             case 95:
-                this.player.replace("Player1",56);
+                this.player.replace(player,56);
                 break;
             case 97:
-                this.player.replace("Player1",78);
+                this.player.replace(player,78);
                 break;
             default:
-                this.player.replace("Player1",position);
+                this.player.replace(player,position);
                 break;
         }
     }
 
-    public void position(){
-        System.out.println("Player at " +this.player.get("Player1"));
+
+    public void position(String player){
+        if(this.player.get(player)>=100){
+            System.out.println(player +" has completed");
+            this.playerStatus.replace(player,0);
+        }else{
+            System.out.println(player+" at " +this.player.get(player));
+        }
+    }
+
+    public int playersList(){
+        return this.player.size();
     }
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,26 +4,32 @@ public class main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter game mode");
+        String mode = sc.nextLine();
+        String[] gameMode = mode.split(" "); // this states how many players are in the game
 
-        SnakesAndLadders game = new SnakesAndLadders();
+        SnakesAndLadders game = new SnakesAndLadders(Integer.parseInt(gameMode[0])); // creates an instance of the gameClass with the game mode as a paramater
+        int player = game.playersList(); // will be used to represent the players playing the game
         boolean exit = true;
 
         while(exit){
-            System.out.println("type play");
-            String move = sc.nextLine();
-            String [] action= move.split(" ");
+            for (int i = 1; i <=player; i++) {
 
-            switch (action[0]){
-                case "play":
-                    game.throwDie();
-                    game.position();
-                    break;
-                case "exit":
-                    exit = false;
-                    break;
-                default:
-                    System.out.println("invalid command");
-                    break;
+                System.out.println("type play");
+                String move = sc.nextLine();
+                String[] action = move.split(" ");
+
+                switch (action[0]) {
+                    case "play":
+                        game.throwDie(i);
+                        break;
+                    case "exit":
+                        exit = false;
+                        break;
+                    default:
+                        System.out.println("invalid command");
+                        break;
+                }
             }
         }
 
